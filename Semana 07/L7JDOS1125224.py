32023da2628ad698a992d4948753bcec5cf4346f
Juandi Sarti <157128349+JuandiS23@users.noreply.github.com>
2024-03-01 08:21:12 -0600

    Add files via upload

---
+++ Semana 07/L7JDOS1125224.py
@@ -0,0 +1,41 @@
+# print("hola mundo")
+# print("ingrese su nombre:")
+# nombre = input()
+
+# print("Mi nombre es:" + nombre)
+
+# # Ejercicio 1
+# print("Ingrese su nombre:")
+# nombre = input()
+# print("Hola " + nombre + "que tengas un dia increible y no te hagas dos horas de trafico")
+
+# # Ejercicio 2
+# print("Ingrese tu edad:")
+# edad = int(input())
+# print("Edad:" + str(edad))
+# print("Hola " + "¡" + nombre + "!" + " tienes " + str(edad) + " años")
+
+# # Ejercicio 3
+# print("Ingrese la temperatura en grados C:")
+# c = int(input())
+# f = 0
+# f = f + c * 9/5 + 32
+# print("La temperatura es F es:" + str(f)) 
+
+# Ejercicio 4
+print("Ingrese el primer numero:")
+num1 = int(input())
+print("Ingrese el segundo numero:")
+num2 = int(input())
+Suma = 0
+Resta = 0
+Multiplicacion = 0
+Division = 0 
+Suma = Suma + (num1 + num2)
+print("El resultado de la suma es:" + str(Suma))
+Resta = Resta + (num1 - num2)
+print("El resultado de la resta es:" + str(Resta))
+Multiplicacion = Multiplicacion + (num1 + num2)
+print("El resultado de la multiplicacion es:" + str(Multiplicacion))
+Division = Division + (num1 / num2)
+print("El resultado de la division es:" + str(Division))
